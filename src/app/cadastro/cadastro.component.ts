faf725a43db49d23013f66fb1411a1ec99dcfee1
matheusmartinz <matheusmartins871@gmail.com>
2025-12-15 22:09:15 -0300

    Inicio cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,21 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, CommonModule, MatCardModule, FormsModule],
+  imports: [
+    FlexLayoutModule,
+    CommonModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
